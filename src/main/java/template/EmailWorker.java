efb642bd865ab4f23d3e872c101485f9fc6edb57
Marcos Brizeno <marcos.uece.comp@gmail.com>
2017-04-02 19:01:08 -0300

    Código inicial para template

--- src/main/java/template/EmailWorker.java
+++ src/main/java/template/EmailWorker.java
@@ -1,19 +1,40 @@
 package template;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class EmailWorker {
+
+    private static Logger logger = Logger.getLogger(EmailWorker.class.getName());
+
+    private ServicoEmail servicoEmail;
+    private int limiteTentativas;
+
+    public EmailWorker(ServicoEmail servicoEmail) {
+        this.servicoEmail = servicoEmail;
+        limiteTentativas = 5;
+    }
+
     public EmailEnviado enviar(int idUsuario, List<String> destinatarios, Email tipoEmail) {
         Usuario usuario = buscarUsuario(idUsuario);
         String corpoEmail = gerarCorpoEmail(tipoEmail, usuario);
         String assunto = gerarAssunto(tipoEmail, usuario);
+        EmailEnviado emailEnviado = new EmailEnviado("", Collections.emptyList());
 
-        return enviarEmail(assunto, corpoEmail, destinatarios);
-    }
-
-    private EmailEnviado enviarEmail(String assunto, String corpoEmail, List<String> destinatarios) {
-        // implementação não importa
-        return new EmailEnviado(assunto, destinatarios);
+        int contadorTentativas = 0;
+        while (contadorTentativas < limiteTentativas) {
+            try {
+                emailEnviado = servicoEmail.enviarEmail(assunto, corpoEmail, destinatarios);
+                contadorTentativas = limiteTentativas;
+            } catch (TimeoutException e) {
+                logger.log(Level.SEVERE, e.getMessage());
+                contadorTentativas++;
+            }
+        }
+        return emailEnviado;
     }
 
     private String gerarAssunto(Email tipoEmail, Usuario usuario) {
--- src/test/java/template/EmailWorkerTest.java
+++ src/test/java/template/EmailWorkerTest.java
@@ -3,9 +3,13 @@ package template;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class EmailWorkerTest {
 
@@ -15,7 +19,48 @@ public class EmailWorkerTest {
         List<String> destinatarios = Arrays.asList("email@email.com", "outro@email.com", "qualquer@email.com");
         String assunto = "Convite enviado por Usuario1";
 
-        EmailWorker emailWorker = new EmailWorker();
+        ServicoEmail servicoEmail = new ServicoEmail();
+        EmailWorker emailWorker = new EmailWorker(servicoEmail);
+        EmailEnviado email = emailWorker.enviar(idUsuario, destinatarios, Email.CONVITE);
+
+        assertEquals(destinatarios, email.getDestinatarios());
+        assertEquals(assunto, email.getAssunto());
+        assertEquals(3, email.getEmailsEnviados());
+    }
+
+    @Test
+    public void trataTimeouts() throws Exception {
+        int idUsuario = 1;
+        List<String> destinatarios = Arrays.asList("email@email.com", "outro@email.com", "qualquer@email.com");
+        String assunto = "Convite enviado por Usuario1";
+        String corpoEmail = "Corpo do email Convite de Usuario1";
+
+        ServicoEmail servicoEmail = mock(ServicoEmail.class);
+        when(servicoEmail.enviarEmail(assunto, corpoEmail, destinatarios)).thenThrow(new TimeoutException("Erro de test"));
+        EmailWorker emailWorker = new EmailWorker(servicoEmail);
+        EmailEnviado email = emailWorker.enviar(idUsuario, destinatarios, Email.CONVITE);
+
+        assertEquals(Collections.emptyList(), email.getDestinatarios());
+        assertEquals("", email.getAssunto());
+        assertEquals(0, email.getEmailsEnviados());
+    }
+
+    @Test
+    public void enviaEmailMesmoApos4Falhas() throws Exception {
+        int idUsuario = 1;
+        List<String> destinatarios = Arrays.asList("email@email.com", "outro@email.com", "qualquer@email.com");
+        String assunto = "Convite enviado por Usuario1";
+        String corpoEmail = "Corpo do email Convite de Usuario1";
+
+        ServicoEmail servicoEmail = mock(ServicoEmail.class);
+        EmailEnviado emailEnviado = new EmailEnviado(assunto, destinatarios);
+        when(servicoEmail.enviarEmail(assunto, corpoEmail, destinatarios))
+                .thenThrow(new TimeoutException("Erro de test"))
+                .thenThrow(new TimeoutException("Erro de test"))
+                .thenThrow(new TimeoutException("Erro de test"))
+                .thenThrow(new TimeoutException("Erro de test"))
+                .thenReturn(emailEnviado);
+        EmailWorker emailWorker = new EmailWorker(servicoEmail);
         EmailEnviado email = emailWorker.enviar(idUsuario, destinatarios, Email.CONVITE);
 
         assertEquals(destinatarios, email.getDestinatarios());
